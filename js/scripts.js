7c64e63971edcea6efc102ca4e801931c22dd0d8
P17188442 <Mattmeakin7@ntlworld.com>
2020-04-17 22:18:01 +0100

    New button on webpage to change homepage to pink and provides log.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 const webShortcut = document.getElementById("Logo");
+const colorchange = document.getElementById("color-change");
 
 function shortcut() {
-  alert.log("You have found a shortcut. You may now go to the home page");
+  alert("You have found a shortcut. You may now go to the home page");
 }
 webShortcut.addEventListener("click", shortcut);
+
+function pink(){
+  console.log("The body of the webpage is now the colour of Leicester's Away Kit")
+  document.body.style.backgroundColor="pink"
+}
+colorchange.addEventListener("click",pink);
