8a1b4008cbeba36d17298234a62e6dac67815b4c
Meckolegend <Mattmeakin7@ntlworld.com>
2020-04-20 21:45:33 +0100

    Tampering with Map settings with different screen sizes

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,11 @@ function shortcut() {
 }
 webShortcut.addEventListener("click", shortcut);
 
-var map = L.map('map').setView([0, 0], 1);
+var map = L.map('map').setView([52.6204, -1.1422], 1);
       L.tileLayer('https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=KWjtmivf2lKbLnZB4srb',{
         tileSize: 512,
         zoomOffset: -1,
-        minZoom: 1,
+        minZoom: 17,
         attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
         crossOrigin: true
       }).addTo(map);
