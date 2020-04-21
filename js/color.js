6cd06c4647fa6ec95b809f351b24b6cbe9948879
Meckolegend <Mattmeakin7@ntlworld.com>
2020-04-21 15:09:05 +0100

    Fixing Color Functions as I had unintentionally removed key features.

--- js/color.js
+++ js/color.js
@@ -1,3 +1,6 @@
+const colorchange = document.getElementById("color-change");
+const colorchange2= document.getElementById("color-change2");
+
 function pink()
 {
   console.log("The body of the webpage is now the colour of Leicester's Away Kit")
@@ -9,5 +12,6 @@ function blue()
 {
   console.log("The body of the webpage is now the colour of Leicester's Home Kit")
   document.body.style.backgroundColor = "teal"
+  document.
 }
 colorchange2.addEventListener("click", blue);
