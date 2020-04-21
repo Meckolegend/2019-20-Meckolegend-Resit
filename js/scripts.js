33a4f7eebff604f4a3060c495040e4754a2604f4
Meckolegend <Mattmeakin7@ntlworld.com>
2020-04-21 14:12:50 +0100

    Fixing up initial script js,removing redundancy and adding const map.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 const webShortcut = document.getElementById("Logo");
-const colorchange = document.getElementById("color-change");
-const colorchange2= document.getElementById("color-change2");
+const map = L.map('map').setView([52.6204, -1.1422], 1);
 
-function shortcut() {
+function shortcut()
+{
   alert("You have found a shortcut. You may now go to the home page");
 }
 webShortcut.addEventListener("click", shortcut);
 
-var map = L.map('map').setView([52.6204, -1.1422], 1);
-      L.tileLayer('https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=KWjtmivf2lKbLnZB4srb',{
-        tileSize: 512,
-        zoomOffset: -1,
-        minZoom: 17,
-        attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
-        crossOrigin: true
-      }).addTo(map);
+
+L.tileLayer('https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=KWjtmivf2lKbLnZB4srb',
+{
+  tileSize: 512,
+  zoomOffset: -1,
+  minZoom: 17,
+  attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
+  crossOrigin: true
+}).addTo(map);
