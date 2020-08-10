5195a0eba5f935da09b5d83b047e01af447f7ca4
Meckolegend <Mattmeakin7@ntlworld.com>
2020-08-10 21:14:56 +0100

    Made changes,realizing I was using Var. Changed const names too.

--- js/slideshow.js
+++ js/slideshow.js
@@ -2,16 +2,16 @@ const slidenumb = 1;
 showDivs(slidenumb);
 
 function plusDivs(n) {
-  showDivs(slidenum += n);
+  showDivs(slidenumb += n);
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("results");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
+  const v;
+  const u = document.getElementsByClassName("results");
+  if (n > u.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = u.length} ;
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    u[i].style.display = "none";
   }
   x[slideIndex-1].style.display = "block";
 }
